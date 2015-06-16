eeee0f847b76609d0b624578b728810f6eb4d73c
Gijsjan <agijsbro@gmail.com>
2015-06-16 11:43:37 +0200

    Change project organisation (add script and folders)

--- build/index.js
+++ build/index.js
@@ -123,56 +123,5 @@ Textarea.propTypes = {
 exports["default"] = Textarea;
 module.exports = exports["default"];
 
-},{"classnames":2,"react":"react"}],2:[function(require,module,exports){
-/*!
-  Copyright (c) 2015 Jed Watson.
-  Licensed under the MIT License (MIT), see
-  http://jedwatson.github.io/classnames
-*/
-
-(function () {
-	'use strict';
-
-	function classNames () {
-
-		var classes = '';
-
-		for (var i = 0; i < arguments.length; i++) {
-			var arg = arguments[i];
-			if (!arg) continue;
-
-			var argType = typeof arg;
-
-			if ('string' === argType || 'number' === argType) {
-				classes += ' ' + arg;
-
-			} else if (Array.isArray(arg)) {
-				classes += ' ' + classNames.apply(null, arg);
-
-			} else if ('object' === argType) {
-				for (var key in arg) {
-					if (arg.hasOwnProperty(key) && arg[key]) {
-						classes += ' ' + key;
-					}
-				}
-			}
-		}
-
-		return classes.substr(1);
-	}
-
-	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(function () {
-			return classNames;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = classNames;
-	} else {
-		window.classNames = classNames;
-	}
-
-}());
-
-},{}]},{},[1])(1)
+},{"classnames":"classnames","react":"react"}]},{},[1])(1)
 });
